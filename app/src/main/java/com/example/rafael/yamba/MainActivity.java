12ebde0f91e81a1153a3626819706e2fbd8279af
RAV-4 <rafael.patinog@gmail.com>
2015-04-16 08:36:26 -0500

    Practica1

---
+++ app/src/main/java/com/example/rafael/yamba/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.rafael.yamba;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.AsyncTask;
+import android.app.Activity;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+    private EditText editStatus;
+    private Button buttonTweet;
+    private TextView textCount;
+    private int defaultTextColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editStatus = (EditText) findViewById(R.id.editStatus);
+        buttonTweet = (Button) findViewById(R.id.buttonTweet);
+        textCount = (TextView) findViewById(R.id.textCount);
+
+        buttonTweet.setOnClickListener(this);
+
+        defaultTextColor = textCount.getTextColors().getDefaultColor();
+
+        buttonTweet.setEnabled(false);
+
+        editStatus.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        int count = 140 - editStatus.length();
+                        if(count < 140 & count > 0){
+                            buttonTweet.setEnabled(true);
+                            buttonTweet.setBackgroundColor(Color.parseColor("#1f1fdf"));
+                            buttonTweet.setTextColor(Color.parseColor("#e9dae9"));
+                        }else{
+                            buttonTweet.setEnabled(false);
+                            buttonTweet.setBackgroundColor(Color.parseColor("#e9dae9"));
+                            buttonTweet.setTextColor(Color.parseColor("#391c52"));
+                        }
+                        textCount.setText(Integer.toString(count));
+                        textCount.setTextColor(Color.DKGRAY);
+                        if(count < 10){
+                            textCount.setTextColor(Color.RED);
+                        }
+                        else{
+                            textCount.setTextColor(defaultTextColor);
+                        }
+
+                    }
+                }
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String status = editStatus.getText().toString();
+        Log.d(TAG, "onClicked with status: " + status);
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editStatus.getWindowToken(), 0);
+
+        editStatus.setText("");
+
+        new PostTask().execute(status);
+    }
+
+    private final class PostTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            YambaClient yambaCloud =
+                    new YambaClient("student", "password");
+
+            try {
+                yambaCloud.postStatus(params[0]);
+                return "Successfully posted";
+            } catch (YambaClientException e) {
+                e.printStackTrace();
+                return "Failed to post to yamba serivce";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
